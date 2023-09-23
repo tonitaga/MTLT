2af38eaba9299b21fadf83e985d30ef130f06a62
tonitaga <onlyforcroyale@gmail.com>
2023-09-23 12:47:53 +0300

    Add matrix access functions

--- src/matrix/include/implementation/matrix.tpp
+++ src/matrix/include/implementation/matrix.tpp
@@ -7,20 +7,20 @@
 
 namespace ng {
     template <typename T>
-    Matrix<T>::Matrix(const Matrix &other)
+    constexpr Matrix<T>::Matrix(const Matrix &other)
         : Matrix(other.rows_, other.cols_) {
         std::copy(other.data_, other.data_ + rows_ * cols_, data_);
     }
 
     template <typename T>
-    Matrix<T>::Matrix(Matrix &&other) noexcept {
+    constexpr Matrix<T>::Matrix(Matrix &&other) noexcept {
         std::swap(rows_, other.rows_);
         std::swap(cols_, other.cols_);
         std::swap(data_, other.data_);
     }
 
     template <typename T>
-    Matrix<T> &Matrix<T>::operator=(const Matrix &other) {
+    constexpr Matrix<T> &Matrix<T>::operator=(const Matrix &other) {
         if (&other == this)
             return *this;
 
@@ -31,7 +31,7 @@ namespace ng {
     }
 
     template <typename T>
-    Matrix<T> &Matrix<T>::operator=(Matrix &&other) noexcept {
+    constexpr Matrix<T> &Matrix<T>::operator=(Matrix &&other) noexcept {
         if (&other == this)
             return *this;
 
@@ -45,6 +45,32 @@ namespace ng {
     template <typename T>
     Matrix<T>::~Matrix() noexcept {
         delete [] data_;
+    }
+
+    template <typename T>
+    typename Matrix<T>::reference Matrix<T>::operator()(size_type row, size_type col) {
+        return data_[row * cols_ + col];
+    }
+
+    template <typename T>
+    typename Matrix<T>::const_reference Matrix<T>::operator()(size_type row, size_type col) const {
+        return data_[row * cols_ + col];
+    }
+
+    template <typename T>
+    typename Matrix<T>::reference Matrix<T>::at(size_type row, size_type col) {
+        if (row >= rows_ or col >= cols_)
+            throw std::out_of_range("row or col is out of range of matrix");
+
+        return (*this)(row, col);
+    }
+
+    template <typename T>
+    typename Matrix<T>::const_reference Matrix<T>::at(size_type row, size_type col) const {
+        if (row >= rows_ or col >= cols_)
+            throw std::out_of_range("row or col is out of range of matrix");
+
+        return (*this)(row, col);
     }
 }
 
--- src/matrix/include/matrix.h
+++ src/matrix/include/matrix.h
@@ -9,27 +9,29 @@ namespace ng {
     template <typename T>
     class Matrix {
         static_assert(std::is_integral_v<T> or std::is_floating_point_v<T>,
-                "Template parameter T must be integral or floating point!");
+                      "Template parameter T must be integral or floating point!");
 
     public:
         using value_type = T;
         using pointer = T *;
+        using reference = T &;
+        using const_reference = const T &;
         using const_pointer = const pointer;
         using size_type = std::size_t;
         using iterator = __internal::MatrixNormalIterator<pointer>;
         using const_iterator = __internal::MatrixNormalIterator<const_pointer>;
 
     public:
-        Matrix() noexcept = default;
+        constexpr Matrix() noexcept = default;
 
-        Matrix(size_type rows, size_type cols) : rows_(rows), cols_(cols), data_(new value_type[rows * cols]{}) {};
-        explicit Matrix(size_type square) : Matrix(square, square) {};
+        constexpr Matrix(size_type rows, size_type cols) : rows_(rows), cols_(cols), data_(new value_type[rows * cols]{}) {};
+        constexpr explicit Matrix(size_type square) : Matrix(square, square) {};
 
-        Matrix(const Matrix &);
-        Matrix(Matrix &&) noexcept;
+        constexpr Matrix(const Matrix &);
+        constexpr Matrix(Matrix &&) noexcept;
 
-        Matrix &operator=(const Matrix &);
-        Matrix &operator=(Matrix &&) noexcept;
+        constexpr Matrix &operator=(const Matrix &);
+        constexpr Matrix &operator=(Matrix &&) noexcept;
 
         ~Matrix() noexcept;
 
@@ -40,10 +42,22 @@ namespace ng {
         const_iterator begin() const noexcept { return const_iterator(data_); }
         const_iterator end() const noexcept { return const_iterator(data_ + rows_ * cols_); }
 
+    public:
+        reference operator()(size_type row, size_type col);
+        const_reference operator()(size_type row, size_type col) const;
+
+        reference at(size_type row, size_type col);
+        const_reference at(size_type row, size_type col) const;
+
     private:
         size_type rows_ {}, cols_ {};
         value_type *data_ = nullptr;
     };
+
+    using IMatrix = Matrix<int>;
+    using UMatrix = Matrix<unsigned>;
+    using FMatrix = Matrix<float>;
+    using DMatrix = Matrix<double>;
 }
 
 #include "matrix.tpp"
