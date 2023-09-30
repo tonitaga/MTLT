ee7679185aac7d7b5a3d0d160fea8d84e327183b
tonitaga <onlyforcroyale@gmail.com>
2023-09-30 11:04:44 +0300

    add to compile_time.cc more examples of compile time computation

--- code_samples/compile_time.cc
+++ code_samples/compile_time.cc
@@ -23,31 +23,59 @@ int main() {
     });
 
     // Also supports mul add sub div operations and other in compile time with matrix
+    constexpr StaticMatrix mul0 = matrix1 * matrix2;
     constexpr StaticMatrix mul1 = matrix1.mul(matrix2);
+
+    constexpr StaticMatrix add0 = matrix1 + matrix2;
     constexpr StaticMatrix add1 = matrix1.add(matrix2);
+
+    constexpr StaticMatrix sub0 = matrix1 - matrix2;
     constexpr StaticMatrix sub1 = matrix1.sub(matrix2);
 
     // If you need to operate your constexpr matrix with some value you can do this:
     constexpr Matrix3x3<int> two(2); // create matrix container only one values
 
     constexpr StaticMatrix mul2 = matrix1.mul_by_element(two);
+    constexpr StaticMatrix mul3 = matrix1 * 2;
+
     constexpr StaticMatrix add2 = matrix1.add(two);
+    constexpr StaticMatrix add3 = matrix1 + 2;
+
     constexpr StaticMatrix sub2 = matrix1.sub(two);
+    constexpr StaticMatrix sub3 = matrix1 - 2;
+
     constexpr StaticMatrix div1 = matrix1.div_by_element(two);
+    constexpr StaticMatrix div2 = matrix1.convert_to<float>() / 2;
 
     // Also you can get transposed matrix, minor matrix, identity
     constexpr StaticMatrix transposed = matrix1.transpose();
     constexpr StaticMatrix minored = matrix1.minor(0, 0);
     constexpr StaticMatrix identity = matrix1.identity();
 
+    // Matrix determinant in compile time
+    constexpr int determinant1 = matrix1.determinant();
+    constexpr int determinant2 = matrix2.determinant();
+
+    // Calculate minor item in compile time
+    constexpr int minor00 = matrix1.minor_item(0, 0);
+    // ... //
+    constexpr int minor22 = matrix1.minor_item(2, 2);
+
+    // Calculate Complements of matrix in compile time
+    constexpr StaticMatrix complements = matrix1.calc_complements();
+
+    // Get Inverse matrix in compile time
+    // Before use you need to convert your matrix to floating_point type
+    constexpr StaticMatrix inverse = matrix1.convert_to<float>().inverse();
+
     // Useful for floating point types
     _GLIBCXX23_CONSTEXPR StaticMatrix round = matrix1.round();
     _GLIBCXX23_CONSTEXPR StaticMatrix floor = matrix1.floor();
     _GLIBCXX23_CONSTEXPR StaticMatrix ceil  = matrix1.ceil();
 
-    // Conversion to is compiled time to
+    // Conversion to array is compiled time too
     constexpr std::array arr1 = matrix1.to_array();
-    constexpr StaticMatrix matrix3 = matrix1.to<float>();
+    constexpr StaticMatrix matrix3 = matrix1.convert_to<float>();
 
     // Also data access is compile time
     constexpr size_type size = matrix1.size();
