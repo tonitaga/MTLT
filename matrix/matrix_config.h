dd7292999b433118bd06b4d53277d17a8979013a
tonitaga (Нурислам Губайдуллин) <108585608+tonitaga@users.noreply.github.com>
2023-10-04 23:36:03 +0300

    Update matrix_config.h

--- matrix/matrix_config.h
+++ matrix/matrix_config.h
@@ -19,14 +19,6 @@
     #endif
 #endif
 
-#ifndef MATRIX_CXX20_CONSTEXPR
-#if __cplusplus > 201703L
-    #define MATRIX_CXX20_CONSTEXPR constexpr
-    #else
-    #define MATRIX_CXX20_CONSTEXPR
-    #endif
-#endif
-
 #ifndef MATRIX_CXX23_CONSTEXPR
 #if __cplusplus > 202002L
     #define MATRIX_CXX23_CONSTEXPR constexpr
