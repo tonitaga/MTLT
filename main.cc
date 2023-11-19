edf11d8358c0fa8070a8a2489cec5d7bb72e31eb
tonitaga <onlyforcroyale@gmail.com>
2023-11-19 16:25:07 +0300

    remove experimental matrices

--- main.cc
+++
@@ -1,3 +0,0 @@
-//
-// Created by onlyf on 14.11.2023.
-//
--- tests/matrix_test.cc
+++ tests/matrix_test.cc
@@ -1,7 +1,6 @@
 #include <gtest/gtest.h>
 
 #include <matrix/matrix.h>
-#include <matrix/row_matrix.h>
 
 using namespace mtl;
 
@@ -567,11 +566,4 @@ TEST(Dynamicmatrix, swap_cols) {
 
     bool equal = m == correct;
     ASSERT_TRUE(equal);
-}
-
-TEST(Dynamicmatrix, operating_with_row_matrix) {
-  row_matrix<int> row_matrix(5, 2);
-  matrix<int> matrix(5, 1, row_matrix);
-
-  ASSERT_EQ(matrix(0, 0), 2);
 }
