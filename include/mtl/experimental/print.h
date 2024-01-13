43f8744c9fa171c9b4e54cb1adbee1416105fcb5
tonitaga <onlyforcroyale@gmail.com>
2024-01-13 18:46:21 +0300

    find errors in implementation for C++17 less standards

--- include/mtl/experimental/print.h
+++ include/mtl/experimental/print.h
@@ -65,7 +65,7 @@ void print(Head &&head, Tail &&...tail) {
 #else
 template<typename Head, typename ...Tail>
 void print(Head &&head, Tail &&...tail) {
-  static_assert(is_printable_v<Head> || is_streamable_v<std::ostream, Head>,
+  static_assert(is_printable<Head>::value || is_streamable<std::ostream, Head>::value,
 				"'Head' template parameter must support << operation with stream, or Head::print(ostream) method");
 #endif
 
@@ -88,7 +88,7 @@ void print(std::ostream &out, Head &&head, Tail &&...tail) {
 #else
 template<typename Head, typename ...Tail>
 void print(std::ostream &out, Head &&head, Tail &&...tail) {
-  static_assert(is_printable_v<Head> || is_streamable_v<std::ostream, Head>,
+  static_assert(is_printable<Head>::value || is_streamable<std::ostream, Head>::value,
 				"'Head' template parameter must support << operation with stream, or Head::print(ostream) method");
 #endif
 
@@ -111,7 +111,7 @@ void print(std::ofstream &out, Head &&head, Tail &&...tail) {
 #else
 template<typename Head, typename ...Tail>
 void print(std::ofstream &out, Head &&head, Tail &&...tail) {
-  static_assert(is_printable_v<Head> || is_streamable_v<std::ofstream, Head>,
+  static_assert(is_printable<Head>::value || is_streamable<std::ofstream, Head>::value,
 				"'Head' template parameter must support << operation with stream, or Head::print(ofstream) method");
 #endif
 
@@ -134,7 +134,7 @@ void print(std::stringstream &out, Head &&head, Tail &&...tail) {
 #else
 template<typename Head, typename ...Tail>
 void print(std::stringstream &out, Head &&head, Tail &&...tail) {
-  static_assert(is_printable_v<Head> || is_streamable_v<std::stringstream, Head>,
+  static_assert(is_printable<Head>::value || is_streamable<std::stringstream, Head>::value,
 				"'Head' template parameter must support << operation with stream, or Head::print(stringstream) method");
 #endif
 
