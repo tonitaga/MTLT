aaa4aea36ce093552d88a91dfe11f501480c2ab8
tonitaga <onlyforcroyale@gmail.com>
2024-01-13 11:45:04 +0300

    copy matrix files to experimental location

---
+++ include/mtl/experimental/matrix_config.h
@@ -0,0 +1,68 @@
+/*
+ *        Copyright 2023, School21 Student Library
+ *        All rights reserved
+ *
+ *        Author:   Gubaydullin Nurislam aka tonitaga
+ *        Email:    gubaydullin.nurislam@gmail.com
+ *        Telegram: @tonitaga
+ *
+ *        The Template Matrix Library for fundamental types
+ *        contains most of the operations on matrices.
+ *
+ *        The Template Matrix library is written in the C++20 standard
+ *        Supports C++11 C++14 C++17 C++20 C++23 versions
+ *
+*/
+
+#ifndef MATRIX_TEMPLATE_LIBRARY_CPP_MATRIX_CONFIG_H_
+#define MATRIX_TEMPLATE_LIBRARY_CPP_MATRIX_CONFIG_H_
+
+#ifndef MATRIX_CXX17_CONSTEXPR
+#  if __cplusplus >= 201703L
+#    define MATRIX_CXX17_CONSTEXPR constexpr
+#  else
+#    define MATRIX_CXX17_CONSTEXPR
+#  endif
+#endif
+
+#ifndef MATRIX_CXX20_CONSTEXPR
+#  if __cplusplus > 201703L
+#    define MATRIX_CXX20_CONSTEXPR constexpr
+#  else
+#    define MATRIX_CXX20_CONSTEXPR
+#  endif
+#endif
+
+#ifndef MATRIX_CXX23_CONSTEXPR
+#  if __cplusplus > 202002L
+#    define MATRIX_CXX23_CONSTEXPR constexpr
+#  else
+#    define MATRIX_CXX23_CONSTEXPR
+#  endif
+#endif
+
+#ifndef MATRIX_CXX17_NODISCARD
+#  if __cplusplus >= 201703L
+#    define MATRIX_CXX17_NODISCARD [[nodiscard]]
+#  else
+#    define MATRIX_CXX17_NODISCARD
+#  endif
+#endif
+
+#ifndef MATRIX_CXX17_UNUSED
+#  if __cplusplus >= 201703L
+#    define MATRIX_CXX17_UNUSED [[maybe_unused]]
+#  else
+#    define MATRIX_CXX17_UNUSED
+#  endif
+#endif
+
+#ifndef MATRIX_CXX17_INLINE
+#  if __cplusplus >= 201703L
+#    define MATRIX_CXX17_INLINE inline
+#  else
+#    define MATRIX_CXX17_INLINE
+#  endif
+#endif
+
+#endif //MATRIX_TEMPLATE_LIBRARY_CPP_MATRIX_CONFIG_H_
--- include/mtl/experimental/matrix_type_traits.h
+++ include/mtl/experimental/matrix_type_traits.h
@@ -2,6 +2,7 @@
 #define MATRIX_TEMPLATE_LIBRARY_EXPERIMENTAL_TYPE_TRAITS_H_
 
 #include <atomic>
+#include <ostream>
 #include <type_traits>
 
 #if __cplusplus > 201703L
@@ -22,7 +23,7 @@ inline constexpr bool is_atomic_v = is_atomic<T>::value;
 #if __cplusplus > 201703L
 
 template <typename T>
-concept atomic = is_atomic<T>::value;
+concept atomic = is_atomic_v<T>;
 
 #endif // __cplusplus > 201703L
 
@@ -37,9 +38,7 @@ struct is_fundamental_atomic {
 } // namespace detail end
 
 template <typename T>
-struct is_fundamental_atomic {
-  static constexpr bool value = detail::is_fundamental_atomic<T>::value;
-};
+struct is_fundamental_atomic : detail::is_fundamental_atomic<T> {};
 
 template <typename T>
 inline constexpr bool is_fundamental_atomic_v = is_fundamental_atomic<T>::value;
@@ -47,9 +46,41 @@ inline constexpr bool is_fundamental_atomic_v = is_fundamental_atomic<T>::value;
 #if __cplusplus > 201703L
 
 template <typename T>
-concept fundamental_atomic = is_fundamental_atomic<T>::value;
+concept fundamental = std::is_fundamental_v<T>;
+
+template <typename T>
+concept fundamental_atomic = is_fundamental_atomic_v<T>;
+
+template <typename T>
+concept fundamental_or_fundamental_atomic = fundamental<T> || fundamental_atomic<T>;
 
 #endif // __cplusplus > 201703L
+
+template <std::size_t Rows, std::size_t Cols>
+struct is_non_zero_dimension : std::false_type {};
+
+template <>
+struct is_non_zero_dimension<0, 0> : std::true_type {};
+
+template <std::size_t Rows, std::size_t Cols>
+inline constexpr bool is_non_zero_dimension_v = is_non_zero_dimension<Rows, Cols>::value;
+
+#if __cplusplus > 201703L
+
+template <std::size_t Rows, std::size_t Cols>
+concept non_zero_dimension = is_non_zero_dimension_v<Rows, Cols>;
+
+#endif // __cplusplus > 201703L
+
+template <typename T>
+struct is_printable {
+  template<typename U>
+  static auto printable_test(const U& u) -> decltype(u.print(), true) {};
+
+  static int printable_test(...) { return 0; };
+
+  static constexpr bool value = (sizeof(printable_test(T{})) == 1);
+};
 
 } // namespace mtl::experimental end
 
